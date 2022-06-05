319b15d2a1e9e0f9c5c85cc2d49d4ee73aa81dd6
Build Agent <buildagent@embracecloud.nl>
2022-06-05 02:35:38 +0200

    Remove ActorId from role entity

--- Migrations/20220604235956_remove-ActorId-from-Role-entity.Designer.cs
+++ Migrations/20220604235956_remove-ActorId-from-Role-entity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTracker;
 
@@ -11,9 +12,10 @@ using MovieTracker;
 namespace MovieTracker.Migrations
 {
     [DbContext(typeof(MovieTrackerContext))]
-    partial class MovieTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20220604235956_remove-ActorId-from-Role-entity")]
+    partial class removeActorIdfromRoleentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Entities/Role.cs
+++ Models/Entities/Role.cs
@@ -3,7 +3,6 @@
 public class Role
 {
     public Guid Id { get; set; }
-    public Guid ActorId { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
     public byte? Score { get; set; }
