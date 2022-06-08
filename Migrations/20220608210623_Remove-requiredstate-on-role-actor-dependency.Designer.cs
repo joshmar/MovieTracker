717261ad287b0495261992821ad6a9f35a72e629
Build Agent <buildagent@embracecloud.nl>
2022-06-08 23:53:29 +0200

    Remove required state on role.actor

--- Migrations/20220608210623_Remove-requiredstate-on-role-actor-dependency.Designer.cs
+++ Migrations/20220608210623_Remove-requiredstate-on-role-actor-dependency.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTracker;
 
@@ -11,9 +12,10 @@ using MovieTracker;
 namespace MovieTracker.Migrations
 {
     [DbContext(typeof(MovieTrackerContext))]
-    partial class MovieTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20220608210623_Remove-requiredstate-on-role-actor-dependency")]
+    partial class Removerequiredstateonroleactordependency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220608210623_Remove-requiredstate-on-role-actor-dependency.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTracker.Migrations
+{
+    public partial class Removerequiredstateonroleactordependency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ActorId",
+                table: "Roles",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ActorId",
+                table: "Roles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
