69b765b9afd78fb3623035724490fe62f2277e71
Build Agent <buildagent@embracecloud.nl>
2022-06-09 12:58:16 +0200

    Add inputtypes

---
+++ GQL/Types/InputTypes/ActorUpdateType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace MovieTracker.GQL.Types.InputTypes;
+
+public class ActorUpdateType : InputObjectGraphType
+{
+    public ActorUpdateType()
+    {
+        Name = "actorInput";
+        Field<NonNullGraphType<StringGraphType>>("firstName");
+        Field<NonNullGraphType<StringGraphType>>("lastName");
+        Field<ByteGraphType>("score");
+    }
+}
